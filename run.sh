b916d5f9ffac819ad9670b8b003437891fe09d71
JiuhaiChen <jiuhaichen@gmail.com>
2025-05-28 19:39:10 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,8 +7,6 @@ export HF_HOME=/HF/Home/
 export OUTPUT_FOLDER=/Your/Model/Output/
 export IMG_FOLDER=/Your/Image/Folder
 
-## import journeyDB folder if you want to use journeyDB, and then you need to add a training argument below   --journeyDB_folder  ${journeyDB_folder}  \  The  journeyDB_folder needs to be the format like:  /fsx/sfr/data/jiuhai/hub/datasets--JourneyDB--JourneyDB/snapshots/e191aa61ca37e5e4418707ade4df5deb5c6d5d8f
-export journeyDB_folder=/Your/JourneyDB/Folder  
 
 torchrun --nproc_per_node=8 \
     blip3o/train/train_mem.py \
